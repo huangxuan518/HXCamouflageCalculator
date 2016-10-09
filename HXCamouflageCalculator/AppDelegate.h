bb50c1cb223346d5648e6e41f104eab97e69070e
huangxuan <770493410@qq.com>
2016-10-09 14:56:26 +0800

    Initial Commit

---
+++ HXCamouflageCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HXCamouflageCalculator
+//
+//  Created by 黄轩 on 16/10/9.
+//  Copyright © 2016年 黄轩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HXCamouflageCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HXCamouflageCalculator
+//
+//  Created by 黄轩 on 16/10/9.
+//  Copyright © 2016年 黄轩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
