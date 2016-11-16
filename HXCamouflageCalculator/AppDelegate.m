5aed1da25bc08b89bba3bd9798dfe5fa19a5100d
huangxuan <770493410@qq.com>
2016-11-16 15:54:10 +0800

    增加效果图

--- HXCamouflageCalculator/AppDelegate.m
+++ HXCamouflageCalculator/AppDelegate.m
@@ -41,7 +41,7 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [UIColor whiteColor];
     
-    [self cutViewController:YES];
+    [self cutViewController:NO];
     [self.window makeKeyAndVisible];
     
     return YES;
@@ -55,8 +55,8 @@
 
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    //APP进入后台
+    [self cutViewController:NO];
 }
 
 
